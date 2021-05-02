11f0c018aa7cb76f5c8efb813643133a062c3b2c
elioth <elioth@elioth-Zephyrus-S-GX531GM-GX531GM>
2021-05-02 22:21:59 +0200

    created django project, updated Dockerfile

--- ngMedPlanner/setup.sh
+++ ngMedPlanner/setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -p 4220:4220 -v ${PWD}/med-planner:/angular --env theport=4220 --name MedPlanner -t -i --rm angular:10.0 npm i exit
+docker run -p 4220:4220 -v ${PWD}/med-planner:/angular --env theport=4220 --name MedPlanner -t -i --rm ng:10.0 npm i exit

26b91eb5369db387c7378d285cb763c1eb517671
andrei <email@example.com>
2021-05-09 21:20:20 +0200

    implemented unit-test for sign-in component

--- ngMedPlanner/med-planner/src/app/templates/app-login/app-login.component.spec.ts
+++ ngMedPlanner/med-planner/src/app/templates/app-login/app-login.component.spec.ts
@@ -22,4 +22,43 @@ describe('AppLoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('email form should be invalid', () => {
+    component.emailFormControl.setValue('someinvalidemailadress');
+
+    expect(component.emailFormControl.valid).toBeFalsy();
+  });
+
+  it('email form should be valid', () => {
+    component.emailFormControl.setValue('some@valid.email');
+
+    expect(component.emailFormControl.valid).toBeTruthy();
+  });
+
+  it('password form should be invalid', () => {
+    component.passwordFormControl.setValue('');
+
+    expect(component.passwordFormControl.valid).toBeFalsy();
+  });
+
+  it('password form should be valid', () => {
+    component.passwordFormControl.setValue('somepassword');
+
+    expect(component.passwordFormControl.valid).toBeTruthy();
+  });
+
+  it('sign-in button should be disabled', () => {
+    fixture.detectChanges();
+    const element = fixture.nativeElement.querySelector('#sing-in-button');
+    expect(element.disabled).toBeTruthy();
+  });
+
+  it('sign-in button should be enabled', () => {
+    component.emailFormControl.setValue('some@valid.email');
+    component.passwordFormControl.setValue('somepassword');
+    fixture.detectChanges();
+
+    const element = fixture.nativeElement.querySelector('#sing-in-button');
+    expect(element.disabled).toBeFalsy();
+  });
 });

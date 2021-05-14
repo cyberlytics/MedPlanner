2149bac43262a4d3c9ecc9029ea34b3b8f33b353
Andrei Trukhin <a.trukhin@oth-aw.de>
2021-05-14 23:03:09 +0200

    Added routing option:
    * login rout
    * appointment-dashboard rout
    
    Added AppHeaderStateService.
    
    Style settings and layout setting for login template.
    
    Former-commit-id: b88ed44674789d074978b92d38a1682f606d5e8d

--- Implementierung/ngMedPlanner/med-planner/src/app/app-header/app-header.component.ts
+++ Implementierung/ngMedPlanner/med-planner/src/app/app-header/app-header.component.ts
@@ -1,5 +1,6 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
+import { AppHeaderStateService } from '../services/state-services/app-header-state.service';
 
 @Component({
   selector: 'app-header-component',
@@ -11,20 +12,44 @@ export class AppHeaderComponent implements OnInit, AfterViewInit {
   @ViewChild('toolbarElement') toolbarElement: MatToolbar | undefined;
   @ViewChild('headerElement') headerElement: ElementRef;
 
-  constructor() {
+  @Output('onHeaderHeightInit')
+  get headerHeightInit(): EventEmitter<number> {
+    return this._headerHeightInit;
+  }
+  private _headerHeightInit: EventEmitter<number>;
+
+  get headerTitle(): string {
+    return this.headerState.headerTitle;
+  }
+
+  get headerSubTitle(): string {
+    return this.headerState.headerSubTitle;
+  }
+
+  constructor(private headerState: AppHeaderStateService) {
     this.headerElement = new ElementRef(null);
+    this._headerHeightInit = new EventEmitter<number>();
   }
 
   ngAfterViewInit(): void {
-    const toolbarHeight = this.toolbarElement?._elementRef.nativeElement.offsetHeight;
+    this.initTopOffsets();
+  }
 
+  ngOnInit(): void {}
+
+  public onWindowResize(): void {
+    this.initTopOffsets();
+  }
+
+  private initTopOffsets(): void {
     try {
+      const toolbarHeight = this.toolbarElement?._elementRef.nativeElement.offsetHeight;
       this.headerElement.nativeElement.style.top = `${toolbarHeight}px`;
+
+      this._headerHeightInit.emit(this.headerElement.nativeElement.offsetHeight + toolbarHeight);
     } catch (e) {
       console.error(e);
     }
   }
-
-  ngOnInit(): void {}
 
 }

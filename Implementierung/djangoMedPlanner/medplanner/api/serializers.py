5a695e0c9c50a97159c120579c922f5423d2db24
elioth <elioth@elioth-Zephyrus-S-GX531GM-GX531GM>
2021-05-15 22:37:10 +0200

    added token generator and serializer for Doctor entity

---
+++ Implementierung/djangoMedPlanner/medplanner/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from medplanner.models import User, Doctor, Appointment
+
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = ["doctor_first_name", "doctor_last_name"]

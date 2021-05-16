6e9ec64c2fa90490d690c3d2ead7533b419a3a09
Egidia Cenko <e.cenko@oth-aw.de>
2021-05-16 17:12:37 +0200

    update entity interfaces
    Former-commit-id: d901b1bf12ab614859aef79e909f86ed469a953f

--- Implementierung/ngMedPlanner/med-planner/src/app/services/data-interfaces/data-interfaces.ts
+++ Implementierung/ngMedPlanner/med-planner/src/app/services/data-interfaces/data-interfaces.ts
@@ -6,3 +6,22 @@ export interface AppointmentMock {
     title: string;
     medic_name: string;
 }
+
+
+// from appointments-list2.json
+export interface AppointmentMock2 {
+    id: number;
+    title: string;
+    datetime: string;
+    priority: string;
+    medic_name: string;
+    note: string;
+}
+
+
+export interface DoctorMock{
+    id: number;
+    first_name: string;
+    surname: string;
+    specialization: string;
+}

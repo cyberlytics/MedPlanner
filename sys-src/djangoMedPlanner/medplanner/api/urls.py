e0e0ba7e29cb0a5774b8ea22749c922c96446f4e
elioth <elioth@elioth-Zephyrus-S-GX531GM-GX531GM>
2021-05-26 22:21:22 +0200

    min code refactoring, update gitignore files, made migrations, added new paths

--- sys-src/djangoMedPlanner/medplanner/api/urls.py
+++ sys-src/djangoMedPlanner/medplanner/api/urls.py
@@ -1,17 +1,21 @@
 from django.urls import path
-from medplanner.api.views import api_create_doctor_view, api_delete_doctor_view, api_all_doctors_view, api_update_doctor_view, api_detail_doctor_view
-from medplanner.api.login import create_user, login, logout
+from medplanner.api.views import api_create_doctor_view, api_delete_doctor_view, api_all_doctors_view, \
+    api_update_doctor_view, api_detail_doctor_view
+from medplanner.api.login import create_user, login, logout, change_user_password, deactivate_user, activate_user
 
 
 app_name = 'medplanner'
 
 urlpatterns = [
     path('doctors', api_all_doctors_view),
-    path('doctor-detail/<pk>/', api_detail_doctor_view, name="detail"),
-    path('new-doctor', api_create_doctor_view, name="create"),
-    path('doctor-delete/<pk>/', api_delete_doctor_view, name="delete"),
-    path('doctor-update/<pk>/', api_update_doctor_view, name="update"),
-    path('new-user', create_user, name="register"),
-    path('login', login, name="login"),
-    path('logout', logout, name="logout"),
+    path('doctor-detail/<pk>/', api_detail_doctor_view, name='detail'),
+    path('new-doctor', api_create_doctor_view, name='create'),
+    path('doctor-delete/<pk>/', api_delete_doctor_view, name='delete'),
+    path('doctor-update/<pk>/', api_update_doctor_view, name='update'),
+    path('new-user', create_user, name='register'),
+    path('login', login, name='login'),
+    path('logout', logout, name='logout'),
+    path('change-password', change_user_password, name='change-password'),
+    path('deactivate-user', deactivate_user, name='deactivate'),
+    path('activate-user', activate_user, name='activate'),
 ]

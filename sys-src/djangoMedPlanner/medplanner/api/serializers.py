f7b05e93518a0809e43401131a2b2982bfd342f8
anonmymous <elioth@elioth-Zephyrus-S-GX531GM-GX531GM>
2021-06-03 19:58:15 +0200

    updated serializer crud services
    Former-commit-id: 1c76d61c4d58eef2b579ef2ef8b052b40067b3cf

--- sys-src/djangoMedPlanner/medplanner/api/serializers.py
+++ sys-src/djangoMedPlanner/medplanner/api/serializers.py
@@ -5,5 +5,4 @@ from medplanner.models import User, Doctor, Appointment
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
-        fields = ['doctor_first_name', 'doctor_last_name', 'specializations', 'surgery_id']
-
+        fields = '__all__'

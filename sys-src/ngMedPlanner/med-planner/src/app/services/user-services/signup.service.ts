5ec9273727ae44e3d8faca039117699422a5156b
Schuster Christoph <c.schuster1@oth-aw.de>
2021-06-10 10:53:51 +0200

    aktueller Stand Registrierung
    Former-commit-id: ad56c501550743619bb170f325c290fa1c1296cb

--- sys-src/ngMedPlanner/med-planner/src/app/services/user-services/signup.service.ts
+++ sys-src/ngMedPlanner/med-planner/src/app/services/user-services/signup.service.ts
@@ -13,31 +13,34 @@ export class SignUpService{
   constructor(private httpService: HttpService, private router: Router) { }
 
   public async signup(_email: string, _password: string): Promise<RegisterResult> {
-    // return this.loginMockCall(_email, _password);
-
-    const response = await this.httpService.postMessage<any>(
+    //console.log(_email,_password);
+    try{
+      const response = await this.httpService.postMessage<any>(
         HttpService.REGISTER_NEW_USER_URL,
         {
           username: _email,
           password: _password
-        }
-    ).catch( (result) => {
-        console.error(result);
+        });
+    }catch (error){
+      console.log(error);
+      if (error.status === HttpService.HTTP_403_FORBIDDEN) { //403
+        console.log("gibt es schon");
+        return RegisterResult.EMAIL_ALREADY_EXIST; 
+      }
+
+      if (error.status === HttpService.HTTP_500_INTERNAL_SERVER_ERROR) {
+        console.log("Server fehler");
         return RegisterResult.SERVER_ERROR;
-    });
-    console.log('response', response);
+      }
+  
+      if (error.status === HttpService.HTTP_500_INTERNAL_SERVER_ERROR) {
+        console.log("Server fehler");
+        return RegisterResult.SERVER_ERROR;
+      }
 
-    if (response.ok === 'false') {
-      console.log("ist falsch");
-      return RegisterResult.EMAIL_ALREADY_EXIST 
     }
 
-    if (response.status === '500') {
-      console.log("Server fehler");
-      return RegisterResult.SERVER_ERROR;
-    }
-
-    return RegisterResult.UNKNOWN_ERROR;
+    return RegisterResult.SIGNUP_SUCCESFULL;
   }
 }
 
@@ -47,6 +50,7 @@ export class SignUpService{
 export enum RegisterResult {
   SIGNUP_SUCCESFULL,
   EMAIL_ALREADY_EXIST,
+  FEHLENDE_DATEN,
   SERVER_ERROR,
   UNKNOWN_ERROR
 }

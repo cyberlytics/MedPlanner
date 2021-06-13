7d177b27a5770e853ac098a3d18d864a3805247f
anonmymous <elioth@elioth-Zephyrus-S-GX531GM-GX531GM>
2021-06-13 10:26:42 +0200

    django: update unit test Former-commit-id: d896b5745fc79df784f8c960a44194378e859785

--- sys-src/djangoMedPlanner/medplanner/api/tests/factories.py
+++ sys-src/djangoMedPlanner/medplanner/api/tests/factories.py
@@ -1,8 +1,9 @@
 from factory import django, Faker
-from medplanner.models import Doctor
+from medplanner.models import Doctor, Surgery, Specialization
 
 
 class DoctorFactory(django.DjangoModelFactory):
+
     doctor_first_name = Faker('first_name')
     doctor_last_name = Faker('last_name')
 

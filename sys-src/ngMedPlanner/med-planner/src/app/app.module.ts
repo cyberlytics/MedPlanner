eb7291471a6762b1d63ae9a5687933b72d698f72
andrei <email@example.com>
2021-06-16 22:00:41 +0200

    Change appointent inputs on model's level

--- sys-src/ngMedPlanner/med-planner/src/app/app.module.ts
+++ sys-src/ngMedPlanner/med-planner/src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppointmentCardComponent } from './templates/appointment-dashboard/appo
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppointmentDashboardComponent } from './templates/appointment-dashboard/appointment-dashboard.component';
-import { AppointmentDetailViewComponent } from './templates/appointment-dashboard/appointment-card/appointment-detail-view/appointment-detail-view.component';
+import { AppointmentDetailViewComponent } from './templates/appointment-dashboard/appointment-card/dialogs/appointment-detail-view/appointment-detail-view.component';
 import { MaterialModule } from './material/material.module';
 import { AppHeaderComponent } from './app-header/app-header.component';
 import { AppLoginComponent } from './templates/app-login/app-login.component';
@@ -24,6 +24,7 @@ import { FilterSpecializationsComponent } from './templates/filter/filter-appoin
 import { FilterCitiesComponent } from './templates/filter/filter-appointments/filter-cities/filter-cities.component';
 import { FilterPeriodComponent } from './templates/filter/filter-appointments/filter-period/filter-period.component';
 import { FilterTagsComponent } from './templates/filter/filter-appointments/filter-tags/filter-tags.component';
+import { AppointmentEditViewComponent } from './templates/appointment-dashboard/appointment-card/dialogs/appointment-edit-view/appointment-edit-view.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ import { FilterTagsComponent } from './templates/filter/filter-appointments/filt
     FilterSpecializationsComponent,
     FilterCitiesComponent,
     FilterPeriodComponent,
-    FilterTagsComponent
+    FilterTagsComponent,
+    AppointmentEditViewComponent
   ],
   imports: [
     BrowserModule,

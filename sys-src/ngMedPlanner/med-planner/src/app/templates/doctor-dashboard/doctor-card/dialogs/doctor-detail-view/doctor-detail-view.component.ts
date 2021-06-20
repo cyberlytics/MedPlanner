a76044c66109a3bfe339a9751c28dd8bf26c65a0
Schuster Christoph <c.schuster1@oth-aw.de>
2021-06-20 14:36:17 +0200

    Doctor-Detailansicht in branch
    Former-commit-id: fff88620606bef917e4607ba43b8de7f02c8e557

---
+++ sys-src/ngMedPlanner/med-planner/src/app/templates/doctor-dashboard/doctor-card/dialogs/doctor-detail-view/doctor-detail-view.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { DoctorModel } from 'src/app/services/state-services/doctors-dashboard/doctor-model';
+import {MatDialog} from '@angular/material/dialog';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DoctorDialog } from 'src/app/templates/doctor-dashboard/doctor-card/dialogs/doctor-dialog';
+
+
+@Component({
+  selector: 'app-doctor-detail-view',
+  templateUrl: './doctor-detail-view.component.html',
+  styleUrls: ['./doctor-detail-view.component.scss']
+})
+export class DoctorDetailViewComponent extends DoctorDialog implements OnInit {
+
+  constructor(
+    private dialogRef: MatDialogRef<DoctorDetailViewComponent, MatDialogRef<DoctorDetailViewComponent>>,
+    @Inject(MAT_DIALOG_DATA) doctor: DoctorModel,
+  ) { 
+    super(doctor);
+  }
+
+  
+  ngOnInit(): void {
+  }
+
+  public closeDialog(editDialogRef?: MatDialogRef<DoctorDetailViewComponent>): void {
+    this.dialogRef.close(editDialogRef);
+  }
+
+}

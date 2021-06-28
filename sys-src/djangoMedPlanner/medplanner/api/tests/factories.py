607df463b7153e67d7bd064ee3f3c2533d51231a
elioth <elioth@elioth-Zephyrus-S-GX531GM-GX531GM>
2021-06-28 17:06:10 +0200

    Create unit tests for models

--- sys-src/djangoMedPlanner/medplanner/api/tests/factories.py
+++ sys-src/djangoMedPlanner/medplanner/api/tests/factories.py
@@ -1,19 +1,53 @@
-from factory import django, Faker
-from medplanner.models import Doctor, Appointment, UserProfile as User
-
-
-class DoctorFactory(django.DjangoModelFactory):
-
-    first_name = Faker('first_name')
-    surname = Faker('last_name')
-
-    class Meta:
-        model = Doctor
+from factory import django, Faker, SubFactory
+from medplanner.models import Doctor, Appointment, UserProfile, Specialization, Surgery
 
 
 class UserProfileFactory(django.DjangoModelFactory):
 
+    class Meta:
+        model = UserProfile
+
     email = Faker('email')
 
+
+class DoctorFactory(django.DjangoModelFactory):
+
     class Meta:
-        model = User
+        model = Doctor
+
+    first_name = Faker('first_name')
+    surname = Faker('last_name')
+    user_id = SubFactory(UserProfileFactory)
+
+
+class AppointmentFactory(django.DjangoModelFactory):
+
+    class Meta:
+        model = Appointment
+
+    title = Faker('first_name')
+    user_id = SubFactory(UserProfileFactory)
+    doc_id = SubFactory(DoctorFactory)
+    datetime = Faker('date_time_ad')
+    priority = "Hoch"
+
+
+class SpecializationFactory(django.DjangoModelFactory):
+
+    class Meta:
+        model = Specialization
+
+    description = Faker('first_name')
+
+
+class SurgeryFactory(django.DjangoModelFactory):
+
+    class Meta:
+        model = Surgery
+
+    address = Faker('street_address')
+    zipcode = Faker('postcode')
+    city = Faker('city')
+    telephone_num = Faker('phone_number')
+    user_id = SubFactory(UserProfileFactory)
+

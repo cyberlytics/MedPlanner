6255be63b5271ccfceed8bc8002e725d2f6a84b7
anonmymous <elioth@elioth-Zephyrus-S-GX531GM-GX531GM>
2021-06-28 17:06:10 +0200

    Create unit tests for models
    Former-commit-id: 29b288de4efe53d2053b7254b799b0c08558772c

--- sys-src/djangoMedPlanner/medplanner/api/tests/test_models.py
+++ sys-src/djangoMedPlanner/medplanner/api/tests/test_models.py
@@ -1,17 +1,43 @@
 from django.test import TestCase
-from medplanner.api.tests.factories import DoctorFactory, UserProfileFactory
-
-
-class DoctorTestCase(TestCase):
-    def test_str(self):
-        """Test for string representation."""
-        doctor = DoctorFactory()
-        self.assertEqual(str(doctor), doctor.doctor_last_name)
+from medplanner.api.tests.factories import DoctorFactory, UserProfileFactory, AppointmentFactory, SurgeryFactory, \
+    SpecializationFactory
 
 
 class UserProfileTestCase(TestCase):
+
     def test_str(self):
         """Test for string representation."""
-        user = UserProfileFactory()
+        user = UserProfileFactory.create()
         self.assertEqual(str(user), user.email)
 
+
+class DoctorTestCase(TestCase):
+
+    def test_str(self):
+        """Test for string representation."""
+        doctor = DoctorFactory.create()
+        self.assertEqual(str(doctor), doctor.surname)
+
+
+class AppointmentTestCase(TestCase):
+
+    def test_str(self):
+        """Test for string representation."""
+        appointment = AppointmentFactory.create()
+        self.assertEqual(str(appointment), appointment.title)
+
+
+class SurgeryTestCase(TestCase):
+
+    def test_str(self):
+        """Test for string representation."""
+        surgery = SurgeryFactory.create()
+        self.assertEqual(str(surgery), surgery.address + ', ' + surgery.zipcode + ' ' + surgery.city)
+
+
+class SpecializationTestCase(TestCase):
+
+    def test_str(self):
+        """Test for string representation."""
+        specialization = SpecializationFactory.create()
+        self.assertEqual(str(specialization), specialization.description)

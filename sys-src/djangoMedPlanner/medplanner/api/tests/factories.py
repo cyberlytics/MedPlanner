260babdef4c3d83425c12859dae38a5b7256ded9
anonmymous <elioth@elioth-Zephyrus-S-GX531GM-GX531GM>
2021-06-29 21:30:01 +0200

    Create unit tests for all models and serializers
    Former-commit-id: 76c939408d34d7fc865f6a3bf450a63e19fb12ad

--- sys-src/djangoMedPlanner/medplanner/api/tests/factories.py
+++ sys-src/djangoMedPlanner/medplanner/api/tests/factories.py
@@ -1,5 +1,5 @@
 from factory import django, Faker, SubFactory
-from medplanner.models import Doctor, Appointment, UserProfile, Specialization, Surgery
+from medplanner.models import Doctor, Appointment, UserProfile, Specialization, Surgery, Tag
 
 
 class UserProfileFactory(django.DjangoModelFactory):
@@ -8,28 +8,7 @@ class UserProfileFactory(django.DjangoModelFactory):
         model = UserProfile
 
     email = Faker('email')
-
-
-class DoctorFactory(django.DjangoModelFactory):
-
-    class Meta:
-        model = Doctor
-
-    first_name = Faker('first_name')
-    surname = Faker('last_name')
-    user_id = SubFactory(UserProfileFactory)
-
-
-class AppointmentFactory(django.DjangoModelFactory):
-
-    class Meta:
-        model = Appointment
-
-    title = Faker('first_name')
-    user_id = SubFactory(UserProfileFactory)
-    doc_id = SubFactory(DoctorFactory)
-    datetime = Faker('date_time_ad')
-    priority = "Hoch"
+    password = Faker('password')
 
 
 class SpecializationFactory(django.DjangoModelFactory):
@@ -50,4 +29,41 @@ class SurgeryFactory(django.DjangoModelFactory):
     city = Faker('city')
     telephone_num = Faker('phone_number')
     user_id = SubFactory(UserProfileFactory)
+
+
+class DoctorFactory(django.DjangoModelFactory):
+
+    class Meta:
+        model = Doctor
+
+    first_name = Faker('first_name')
+    surname = Faker('last_name')
+    user_id = SubFactory(UserProfileFactory)
+    surgery_id = SubFactory(SurgeryFactory)
+
+
+class TagFactory(django.DjangoModelFactory):
+
+    class Meta:
+        model = Tag
+
+    description = Faker('first_name')
+    color = Faker('color')
+    user_id = SubFactory(UserProfileFactory)
+
+
+class AppointmentFactory(django.DjangoModelFactory):
+
+    class Meta:
+        model = Appointment
+
+    title = Faker('first_name')
+    user_id = SubFactory(UserProfileFactory)
+    doc_id = SubFactory(DoctorFactory)
+    datetime = Faker('date_time_ad')
+    priority = "Hoch"
+    note = Faker('email')
+
+
+
 

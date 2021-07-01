60784d4c4da64db17d092755a129f0c8974974a4
Matthias Schön <m.schoen@oth-aw.de>
2021-07-02 00:28:33 +0200

    code format correction
    Former-commit-id: 525b820223da84e456a42ccd07f5e2ae345fb84c

--- sys-src/djangoMedPlanner/medplanner/api/user_view.py
+++ sys-src/djangoMedPlanner/medplanner/api/user_view.py
@@ -2,7 +2,7 @@ from django.contrib import auth
 from rest_framework.authtoken.models import Token
 from medplanner.models import UserProfile as User
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated  
 from rest_framework.response import Response
 from rest_framework import status
 from django.contrib import messages

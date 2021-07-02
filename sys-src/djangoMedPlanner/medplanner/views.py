74505f93da3a4289ffaa47b443b4b29b277d4065
Egidia Cenko <e.cenko@oth-aw.de>
2021-07-02 12:35:38 +0200

    update email notification and comments
    Former-commit-id: 926f2b0bdad0dc39004960fe309d16ee1798a1b7

--- sys-src/djangoMedPlanner/medplanner/views.py
+++ sys-src/djangoMedPlanner/medplanner/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 # start page with dummy content
- def index(request):
-   return HttpResponse("Welcome to MedPlanner! Go to /admin to use admin mode.")
+def index(request):
+  return HttpResponse("Welcome to MedPlanner! Go to /admin to use admin mode.")
